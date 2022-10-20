1bc948c54498d5b386f89329df814257da864d90
Amir <73050351+Amiir-zar@users.noreply.github.com>
2022-10-20 23:37:12 +0330

    Update HI_SQL_project.sql

--- HI_SQL_project.sql
+++ HI_SQL_project.sql
@@ -1,25 +1,29 @@
--- Q1 --
+-- Q1
+
 SELECT AccountNumber
 FROM [Purchasing].[Vendor]
 WHERE BusinessEntityID = 1512
 -----------------------------------------------------------------
 -- Q2
+
 SELECT COUNT(*) AS 'total_number of Design Engineer'
 FROM [HumanResources].[Employee]
 WHERE JobTitle = 'Design Engineer'
--- to see the list 
+-- list:
 SELECT *
 FROM [HumanResources].[Employee]
 WHERE JobTitle = 'Design Engineer'
 
 -----------------------------------------------------------------
 -- Q3
+
 SELECT *
 FROM [HumanResources].[Employee]
 WHERE BirthDate IN ('1969-01-29','1974-11-12','1984-11-30','1987-05-03'); 
 
 -----------------------------------------------------------------
 -- Q4
+
 SELECT ContactTypeID
 FROM Person.ContactType
 WHERE Name = 'Accounting Manager';
@@ -32,13 +36,15 @@ WHERE Name = 'Accounting Manager';
 -- equal to 16.
 
 -----------------------------------------------------------------
--- Q6 
+-- Q6
+
 SELECT COUNT(BusinessEntityID)
 FROM [HumanResources].[Employee]
 WHERE JobTitle = 'Design Engineers'
 
 -----------------------------------------------------------------
 -- Q7
+
 SELECT COUNT(*)
 FROM [HumanResources].[Employee]
 WHERE JobTitle LIKE '%Design%'
@@ -49,8 +55,8 @@ FROM [HumanResources].[Employee]
 WHERE JobTitle LIKE '%Design%'
 
 -----------------------------------------------------------------
-
 -- Q8 ( Adding Mr.Carl Rickman)
+
 SET IDENTITY_INSERT [AdventureWorks2019].[Person].[ContactType] ON;
 INSERT INTO [Person].[ContactType] (ContactTypeID,Name, ModifiedDate )
 VALUES(21,'Data Analyst', GETDATE()); -- CAST() if we only need the Date 
@@ -60,6 +66,7 @@ SELECT *
 FROM [Person].[ContactType] ; 
 -----------------------------------------------------------------
 -- Q9 
+
 INSERT INTO [Person].[ContactType] (ContactTypeID,Name, ModifiedDate) 
 VALUES ( 22, 'Data Analyst', '2008-04-30 00:00:00.000');
 
@@ -71,16 +78,16 @@ VALUES ( 22, 'Data Analyst', '2008-04-30 00:00:00.000');
 
 -----------------------------------------------------------------
 -- Q10 
+
 -- first lets look at the data that we should change:
 SELECT * 
 FROM Sales.SpecialOffer
 WHERE [Description] = 'Mountain Tire Sale' ; 
 
--- now lets look whether it changed:
+-- Now lets chage it:
 UPDATE Sales.SpecialOffer
 SET [DiscountPct] = [DiscountPct] + 0.25 
 WHERE [Description] = 'Mountain Tire Sale';
--- yay!
 
 -----------------------------------------------------------------
 -- Q11
@@ -89,12 +96,12 @@ SELECT AVG(DiscountPct)
 FROM Sales.SpecialOffer;
 -----------------------------------------------------------------
 -- Q12
+
 SELECT TOP(1)   [Name] ,[TaxType] , [TaxRate]
 FROM [Sales].[SalesTaxRate]
 ORDER BY TaxRate DESC ; 
 
 -----------------------------------------------------------------
-
 -- Q13
 
 DELETE
@@ -105,24 +112,26 @@ WHERE SalesTaxRateID = 1
 SELECT * FROM [Sales].SalesTaxRate ;
 -- YEAH! There is no Tax with SalesTaxRateID = 1
 -----------------------------------------------------------------
-
 -- Q14
+
 SELECT AccountNumber
 FROM [Purchasing].[Vendor]
 WHERE Name = 'Cycling Master';
 -----------------------------------------------------------------
 -- Q15
+
 SELECT Name, ReasonType
 FROM Sales.SalesReason;
 -----------------------------------------------------------------
 -- Q16 
+
 SELECT TOP(10) *
 FROM Purchasing.Vendor
 WHERE CreditRating = 2
 ORDER BY ModifiedDate ; 
 -----------------------------------------------------------------
-
 -- Q17
+
 IF EXISTS ( SELECT * FROM Person.Person WHERE BusinessEntityID = 3 )
 BEGIN 
 UPDATE Person.Person
@@ -135,7 +144,8 @@ SELECT TOP(10) *
 FROM Person.Person;
 
 -----------------------------------------------------------------
--- Q18 -- 
+-- Q18 
+
 IF EXISTS ( SELECT * FROM Person.Person WHERE FirstName = 'Terri')
 BEGIN
 DELETE FROM Person.Person WHERE  FirstName = 'Terri' ; 
@@ -144,13 +154,14 @@ END
 -- The answer is : It's not possible to delete this name because it is a foreign key for another table ( EmailAddress)
 -----------------------------------------------------------------
 -- Q19 
+
 SELECT COUNT(TaxRate) AS TOTAL_NUMBER,
 	   AVG(TaxRate) AS AVERAGE,
        SUM(TaxRate) AS SUM   
 FROM [Sales].SalesTaxRate 
 -----------------------------------------------------------------
-
 -- Q20 
+
 SELECT p.FirstName , p.LastName , pe.EmailAddress
 FROM Person.Person p
 JOIN Person.EmailAddress pe 
