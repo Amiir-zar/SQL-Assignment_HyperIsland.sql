3eedf7df20a3201616f604385e9e35e85bfe5c21
Amir <73050351+Amiir-zar@users.noreply.github.com>
2022-10-23 11:24:41 +0330

    Update HI_SQL_project.sql

--- HI_SQL_project.sql
+++ HI_SQL_project.sql
@@ -9,6 +9,7 @@ WHERE BusinessEntityID = 1512
 SELECT COUNT(*) AS 'total_number of Design Engineer'
 FROM [HumanResources].[Employee]
 WHERE JobTitle = 'Design Engineer'
+
 -- list:
 SELECT *
 FROM [HumanResources].[Employee]
@@ -30,17 +31,21 @@ WHERE Name = 'Accounting Manager';
 -----------------------------------------------------------------
 
 -- Q5--
--- It's pretty simple and slelf explanitory : 
+-- It's pretty simple and self explanatory : 
 -- the commend will select three columns BusinessEntityID, Rate, PayFrequency from the 
--- [EmployeePayHistory] table only when PayFrequency is less than 22 AND BusinessEntityID is 
--- equal to 16.
+-- [EmployeePayHistory] table only when PayFrequency is less than 22 AND BusinessEntityID is equal to 16.
 
 -----------------------------------------------------------------
 -- Q6
 
-SELECT COUNT(BusinessEntityID)
+SELECT COUNT(*) AS 'total_number of Design Engineer'
 FROM [HumanResources].[Employee]
-WHERE JobTitle = 'Design Engineers'
+WHERE JobTitle = 'Design Engineer'
+
+-- list:
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE JobTitle = 'Design Engineer'
 
 -----------------------------------------------------------------
 -- Q7
@@ -55,15 +60,12 @@ FROM [HumanResources].[Employee]
 WHERE JobTitle LIKE '%Design%'
 
 -----------------------------------------------------------------
--- Q8 ( Adding Mr.Carl Rickman)
+-- Q8 
 
-SET IDENTITY_INSERT [AdventureWorks2019].[Person].[ContactType] ON;
 INSERT INTO [Person].[ContactType] (ContactTypeID,Name, ModifiedDate )
-VALUES(21,'Data Analyst', GETDATE()); -- CAST() if we only need the Date 
+VALUES(21,'Data Analyst', GETDATE());
+-- CAST() if we only need the Date 
 
--- Now lets check whether is is updated or not:
-SELECT *
-FROM [Person].[ContactType] ; 
 -----------------------------------------------------------------
 -- Q9 
 
@@ -73,18 +75,20 @@ VALUES ( 22, 'Data Analyst', '2008-04-30 00:00:00.000');
 -- Error : Cannot insert duplicate key row in object 'Person.ContactType' with unique index 'AK_ContactType_Name'. 
 -- The duplicate key value is (Data Analyst).
 
--- Answer : It's obvious becuase the NAME column is UNIQUE and since we already HAVE the Data Analyst 
+-- Reason : It's obvious becuase the NAME column is UNIQUE and since we already HAVE the Data Analyst 
 --          in that so it is not possible to have another one.
 
+-- Solution : 
+   SET IDENTITY_INSERT [Person].[ContactType] ON    	   -- To be able to insert data on that table 
+   DROP INDEX AK_ContacType_Name ON [Person].[ContactType] -- To be able have similar value in different rows
+   
+   INSERT INTO [Person].[ContactType] (ContactTypeID,Name, ModifiedDate) 
+   VALUES ( 22, 'Data Analyst', '2008-04-30 00:00:00.000');
+
+   SET IDENTITY_INSERT Person.ContactType OFF;
 -----------------------------------------------------------------
 -- Q10 
 
--- first lets look at the data that we should change:
-SELECT * 
-FROM Sales.SpecialOffer
-WHERE [Description] = 'Mountain Tire Sale' ; 
-
--- Now lets chage it:
 UPDATE Sales.SpecialOffer
 SET [DiscountPct] = [DiscountPct] + 0.25 
 WHERE [Description] = 'Mountain Tire Sale';
@@ -97,9 +101,14 @@ FROM Sales.SpecialOffer;
 -----------------------------------------------------------------
 -- Q12
 
-SELECT TOP(1)   [Name] ,[TaxType] , [TaxRate]
-FROM [Sales].[SalesTaxRate]
+SELECT TOP(1)  [Name] ,[TaxType] , [TaxRate]
+FROM Sales.SalesTaxRate
 ORDER BY TaxRate DESC ; 
+
+-- Other Way : 
+SELECT  [Name] ,[TaxType] , [TaxRate]
+FROM Sales.SalesTaxRate
+WHERE TaxRate = (SELECT MAX(TaxRate) FROM Sales.SalesTaxRate);
 
 -----------------------------------------------------------------
 -- Q13
@@ -108,9 +117,6 @@ DELETE
 FROM [Sales].SalesTaxRate 
 WHERE SalesTaxRateID = 1 
 
--- check it :
-SELECT * FROM [Sales].SalesTaxRate ;
--- YEAH! There is no Tax with SalesTaxRateID = 1
 -----------------------------------------------------------------
 -- Q14
 
@@ -128,7 +134,7 @@ FROM Sales.SalesReason;
 SELECT TOP(10) *
 FROM Purchasing.Vendor
 WHERE CreditRating = 2
-ORDER BY ModifiedDate ; 
+ORDER BY ModifiedDate ;  -- by saying " 10 first"  I assume that we should show them by the time they have been registered 
 -----------------------------------------------------------------
 -- Q17
 
@@ -155,15 +161,17 @@ END
 -----------------------------------------------------------------
 -- Q19 
 
-SELECT COUNT(TaxRate) AS TOTAL_NUMBER,
-	   AVG(TaxRate) AS AVERAGE,
-       SUM(TaxRate) AS SUM   
+SELECT 
+	COUNT(TaxRate) AS TOTAL_NUMBER,
+	AVG(TaxRate) AS AVERAGE,
+	SUM(TaxRate) AS SUM   
 FROM [Sales].SalesTaxRate 
+
 -----------------------------------------------------------------
 -- Q20 
 
-SELECT p.FirstName , p.LastName , pe.EmailAddress
-FROM Person.Person p
+SELECT pp.FirstName , pp.MiddleName, pp.LastName , pe.EmailAddress
+FROM Person.Person pp
 JOIN Person.EmailAddress pe 
-ON pe.BusinessEntityID = p.BusinessEntityID  -- Q: is it possible to use USING(BusinessEntityID)?
-WHERE p.MiddleName is NULL ;
+ON pe.BusinessEntityID = pp.BusinessEntityID  -- Q: is it possible to use USING(BusinessEntityID)?
+WHERE pp.MiddleName is NULL ;
